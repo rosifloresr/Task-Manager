88f8d899d1a6ff906bbf1c22806b5bed58e2f947
rosifloresr <floresmrosi@gmail.com>
2026-04-03 19:09:14 -0300

    version local correcta

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,6 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule); // Asegúrate de que el nombre del módulo coincida
-  await app.listen(3001); // Puerto 3001 para Auth
+  const app = await NestFactory.create(AuthModule); 
+  await app.listen(3001); 
 }
